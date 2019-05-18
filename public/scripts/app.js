34c9e4fce3ccde4ca93cc67658ba7a6aaa535bad
gitvijayy <7822vijay@gmail.com>
2019-05-17 21:17:53 -0700

    formatting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery`s document ready function
- */
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery`s document ready function
- */
 $(document).ready(() => {
 
   //to calculate the date in days years etc.. source:StackOverFlow
