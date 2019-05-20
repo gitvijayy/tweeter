934c445b43ac2e422c239e0b57aef147fed725ea
gitvijayy <7822vijay@gmail.com>
2019-05-20 02:51:33 +0000

    heroku cache

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const app = express();
 const MongoClient = require(`mongodb`).MongoClient;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb://heroku_1z3jsnrc:fka36mot8qft09pjd9gppd0j2r@ds151076.mlab.com:51076/heroku_1z3jsnrc";
 //mongodb://heroku_1z3jsnrc:fka36mot8qft09pjd9gppd0j2r@ds151076.mlab.com:51076/heroku_1z3jsnrc
 
 app.use(bodyParser.urlencoded({ extended: true }));
